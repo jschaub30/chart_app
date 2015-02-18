421480ac6a532093ff185c49a9eea1712bced757
Jeremy Schaub <schaubjeremy@gmail.com>
2015-02-18 09:41:55 -0600

    Before model change

--- orgchart/views.py
+++ orgchart/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Org
+from .models import Org, Chart
 
 class OrgList(ListView):
+    model = Org
+    template_name = 'orgchart/org_list.html'
+
+class ChartList(ListView):
     model = Org
     template_name = 'orgchart/org_list.html'
 
